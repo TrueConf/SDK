dea6a848aaf9681616db239cab529a4b9776dccf
Mariia Khrapunova <khrapunova@trueconf.com>
2018-01-11 19:03:16 +0200

    update CallUserById example

--- Windows/csharp-examples/CallUserById/DemoCallUserById/Form1.cs
+++ Windows/csharp-examples/CallUserById/DemoCallUserById/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Helpers;
 using System.Windows.Forms;
 
 namespace DemoSDK
@@ -15,24 +16,71 @@ namespace DemoSDK
         public static string ServerName;
         public static string TrueConf_Id;
         public static string TrueConf_Password;
-        private bool isCanCall = false; //flag means the CallX can make calls
         private bool started = false; //flag means that TrueConf CallX started
+        private bool isCalling; //flag means the CallX is calling now
+
         public Form1()
         {
             Auth auth = new Auth(); //open authorization dialog after form is initialized
             auth.ShowDialog(); //open authorization dialog after form is initialized        
-            InitializeComponent();                
+            InitializeComponent();
+            axTrueConfCallX1.OnServerConnected += axTrueConfCallX1_OnServerConnected; //handle OnServerConnected event
+            axTrueConfCallX1.OnXAfterStart += axTrueConfCallX1_OnXAfterStart; //handle OnXAfterStart event
+            axTrueConfCallX1.OnXLogin += axTrueConfCallX1_OnXLogin; //handle OnXLogin event
+            axTrueConfCallX1.OnXLoginError += axTrueConfCallX1_OnXLoginError; //handle OnXLoginError event
+            axTrueConfCallX1.OnConferenceCreated += axTrueConfCallX1_OnConferenceCreated; //handle OnConferenceCreated event
+            axTrueConfCallX1.OnInviteReceived += AxTrueConfCallX1_OnInviteReceived; //handle OnInviteReceived event
+            axTrueConfCallX1.OnServerDisconnected += AxTrueConfCallX1_OnServerDisconnected; //handle OnServerDisconnected event
+            axTrueConfCallX1.OnLogout += AxTrueConfCallX1_OnLogout; //handle OnLogout event
+            axTrueConfCallX1.OnConferenceDeleted += AxTrueConfCallX1_OnConferenceDeleted; //handle OnConferenceDeleted event
         }
+
+        private void AxTrueConfCallX1_OnConferenceDeleted(object sender, AxTrueConf_CallXLib._ITrueConfCallXEvents_OnConferenceDeletedEvent e)
+        {
+            toolStripStatusConference.Text = "Not in conference";
+        }
+
+        private void AxTrueConfCallX1_OnLogout(object sender, AxTrueConf_CallXLib._ITrueConfCallXEvents_OnLogoutEvent e)
+        {
+            toolStripStatusLogin.Text = "Not logged in";
+        }
+
+        private void AxTrueConfCallX1_OnServerDisconnected(object sender, AxTrueConf_CallXLib._ITrueConfCallXEvents_OnServerDisconnectedEvent e)
+        {
+            toolStripStatusServer.Text = "Not connected";
+        }
+
+        private void AxTrueConfCallX1_OnInviteReceived(object sender, AxTrueConf_CallXLib._ITrueConfCallXEvents_OnInviteReceivedEvent e)
+        {
+            dynamic data = Json.Decode(e.eventDetails); //get information about incoming call from JSON data
+            string message = "";
+            switch (data.type)
+            {
+                case 0: //type 0 - peer-to-peer call
+                    message = "Do you want accept call from ";
+                    break;
+                case 1: //type 1 - conference
+                    message = "Do you want accept invite to conference from ";
+                    break;
+            }
+            DialogResult result = MessageBox.Show(message + data.peerId + "?", "Incoming call", MessageBoxButtons.YesNo); //ask user
+            if (result == DialogResult.Yes)
+                axTrueConfCallX1.accept(); //if user says "Yes" - accept incoming call
+            else
+                axTrueConfCallX1.reject();// else - reject incoming call
+        }
+
         private void axTrueConfCallX1_OnServerConnected(object sender, AxTrueConf_CallXLib._ITrueConfCallXEvents_OnServerConnectedEvent e) //notification about success connection to server
         {
-            MessageBox.Show("Connect to server ok!");
+            toolStripStatusServer.Text = "Connected to " + ServerName; //show server status
             if(!String.IsNullOrEmpty(TrueConf_Id) && !String.IsNullOrEmpty(TrueConf_Password))
                 axTrueConfCallX1.login(TrueConf_Id, TrueConf_Password); //login on TrueConf Server with login and password
         }
-        private void axTrueConfCallX1_OnXLoginOk(object sender, EventArgs e) //notification about successed login
+        private void axTrueConfCallX1_OnXLogin(object sender, EventArgs e) //notification about successed login
         {
-            MessageBox.Show("Login ok!"); //show message about successed login
-            isCanCall = true; //flag means the CallX can make calls
+            Enabled = true;
+            toolStripStatusLogin.Text = "You are logged in as " + TrueConf_Id; //show login status
+            isCalling = false; //flag means the CallX can make calls
             for (int i = 0; i < axTrueConfCallX1.XGetCameraCount(); i++)
                 comboBoxCameras.Items.Add(axTrueConfCallX1.XGetCameraByIndex(i)); //get cameras and put them to combobox
             for (int i = 0; i < axTrueConfCallX1.XGetMicCount(); i++)
@@ -42,14 +90,18 @@ namespace DemoSDK
         }
         private void axTrueConfCallX1_OnXLoginError(object sender, AxTrueConf_CallXLib._ITrueConfCallXEvents_OnXLoginErrorEvent e)
         {
-            MessageBox.Show("Login error!"); //show message about error login
+            toolStripStatusLogin.Text = "Login error!"; //show login status
             Auth auth = new Auth(); //open authorization dialog again
             auth.ShowDialog();
         }
         private void axTrueConfCallX1_OnXAfterStart(object sender, EventArgs e)
         {
             started = true; //flag means TrueConf CallX started
-            if(ServerName != null)
+            toolStripStatusServer.Text = "Not connected";
+            toolStripStatusLogin.Text = "Not logged in";
+            toolStripStatusConference.Text = "Not in conference";
+            Enabled = false;
+            if (ServerName != null)
             {
                 axTrueConfCallX1.connectToServer(ServerName); //after start connect to the server/service
             }
@@ -71,24 +123,25 @@ namespace DemoSDK
         }
         private void buttonCall_Click(object sender, EventArgs e)
         {
-            if (textBoxTrueConfIdForCall.Text != "" && isCanCall) //if trueconf id entered, and CallX is connected to server and authorized and isn't in call or conference
+            if (textBoxTrueConfIdForCall.Text != "" && !isCalling) //if trueconf id entered, and CallX is connected to server and authorized and isn't in call or conference
             {
+                isCalling = true;
                 axTrueConfCallX1.call(textBoxTrueConfIdForCall.Text); //call the entered user                
                 textBoxTrueConfIdForCall.Text = ""; //clear the textbox
             }
         }
         private void buttonEndCall_Click(object sender, EventArgs e)
         {
-            if (!isCanCall) //if Callx is in call
+            if (isCalling) //if Callx is in call
             {
                 axTrueConfCallX1.hangUp(); //end the call
-                isCanCall = true; //and set that the CallX can make the calls
+                isCalling = false; //and set that the CallX can make the calls
             }
         }
         
         private void axTrueConfCallX1_OnConferenceCreated(object sender, AxTrueConf_CallXLib._ITrueConfCallXEvents_OnConferenceCreatedEvent e)
         {
-            isCanCall = false; //set that CallX cannot make calls
+            toolStripStatusConference.Text = "In conference"; //show conference status
         }
     }
 }
